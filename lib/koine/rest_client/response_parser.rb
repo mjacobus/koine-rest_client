5cf20a22d0d8f855aac5c0d0c2122e18ef3bf50a
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-08-16 17:17:44 -0300

    Add new kinds of errors

--- lib/koine/rest_client/response_parser.rb
+++ lib/koine/rest_client/response_parser.rb
@@ -24,8 +24,34 @@ module Koine
         {
           400 => BadRequestError,
           401 => UnauthorizedError,
+          402 => PaymentRequiredExperimentalError,
+          403 => ForbiddenError,
           404 => NotFoundError,
-          500 => InternalServerError
+          405 => MethodNotAllowedError,
+          406 => NotAcceptableError,
+          407 => ProxyAuthenticationRequiredError,
+          408 => RequestTimeoutError,
+          409 => ConflictError,
+          410 => GoneError,
+          411 => LengthRequiredError,
+          412 => PreconditionFailedError,
+          413 => PayloadTooLargeError,
+          414 => URITooLongError,
+          415 => UnsupportedMediaTypeError,
+          416 => RangeNotSatisfiableError,
+          417 => ExpectationFailedError,
+          418 => ImATeapotError,
+          500 => InternalServerError,
+          501 => NotImplementedError,
+          502 => BadGatewayError,
+          503 => ServiceUnavailableError,
+          504 => GatewayTimeoutError,
+          505 => HTTPVersionNotSupportedError,
+          506 => VariantAlsoNegotiatesError,
+          507 => InsufficientStorageError,
+          508 => LoopDetectedError,
+          510 => NotExtendedError,
+          511 => NetworkAuthenticationRequiredError
         }.fetch(code) { Error }
       end
     end
