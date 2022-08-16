b279dfcee086ab66ca0dd941f296bc61f0c96da4
Marcelo Jacobus <marcelo.jacobus@gmail.com>
2022-08-16 17:17:44 -0300

    Add new kinds of errors

--- lib/koine/rest_client/errors.rb
+++ lib/koine/rest_client/errors.rb
@@ -11,9 +11,38 @@ module Koine
       end
     end
 
+    # 400
     BadRequestError = Class.new(Error)
     NotFoundError = Class.new(Error)
     UnauthorizedError = Class.new(Error)
+    PaymentRequiredExperimentalError = Class.new(Error)
+    ForbiddenError = Class.new(Error)
+    MethodNotAllowedError = Class.new(Error)
+    NotAcceptableError = Class.new(Error)
+    ProxyAuthenticationRequiredError = Class.new(Error)
+    RequestTimeoutError = Class.new(Error)
+    ConflictError = Class.new(Error)
+    GoneError = Class.new(Error)
+    LengthRequiredError = Class.new(Error)
+    PreconditionFailedError = Class.new(Error)
+    PayloadTooLargeError = Class.new(Error)
+    URITooLongError = Class.new(Error)
+    UnsupportedMediaTypeError = Class.new(Error)
+    RangeNotSatisfiableError = Class.new(Error)
+    ExpectationFailedError = Class.new(Error)
+    ImATeapotError = Class.new(Error)
+
+    # 500
     InternalServerError = Class.new(Error)
+    NotImplementedError = Class.new(Error)
+    BadGatewayError = Class.new(Error)
+    ServiceUnavailableError = Class.new(Error)
+    GatewayTimeoutError = Class.new(Error)
+    HTTPVersionNotSupportedError = Class.new(Error)
+    VariantAlsoNegotiatesError = Class.new(Error)
+    InsufficientStorageError = Class.new(Error)
+    LoopDetectedError = Class.new(Error)
+    NotExtendedError = Class.new(Error)
+    NetworkAuthenticationRequiredError = Class.new(Error)
   end
 end
